1909b31c81ece912231e23ce26410d91160a4244
Antoine80700 <89512337+Antoine80700@users.noreply.github.com>
2024-07-27 18:41:57 +0200

    1.0.1

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,4 @@
 /**
- * @author Luuxis
  * @license CC-BY-NC 4.0 - https://creativecommons.org/licenses/by-nc/4.0
  */
 
@@ -30,9 +29,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Je... vie...", "author": "Antoine" },
+            { "message": "Salut je suis du code.", "author": "Antoine" },
+            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Antoine" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
